a3967491f202c09de1f98241099d77e7e448d184
dindacha <dindacha3@gmail.com>
2023-11-14 17:24:31 +0700

    modul 3

---
+++ MODUL3 DINDA/connect.php
@@ -0,0 +1,14 @@
+<!-- File ini berisi koneksi dengan database yang telah di import ke phpMyAdmin kalian -->
+
+
+<?php
+// Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+
+// 
+
+    $connect = mysqli_connect("localhost","root","dindacha13","db_modul3wad");
+
+// Buatlah perkondisian jika tidak bisa terkoneksi ke database maka akan mengeluarkan errornya
+
+// 
+?>
